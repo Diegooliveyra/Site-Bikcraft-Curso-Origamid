95a094000d756161c9dc25c3171d740f9d4fb2d1
Diego Oliveira <54894831+Diegooliveyra@users.noreply.github.com>
2020-03-09 13:04:16 -0300

    Adc animaçoes em JS

--- js/main.js
+++ js/main.js
@@ -10,3 +10,21 @@ $(".rslides").responsiveSlides({
     timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
     pager: true,
   });
+
+  Visibility.onVisible(function(){
+    setTimeout(function(){
+      $(".introducao h1, .introducao-interna h1").addClass("animated fadeInDown");
+    },400);
+    setTimeout(function(){
+      $(".quote-externo, .introducao-interna p").addClass("animated fadeInDown");
+    },800);
+    setTimeout(function(){
+      $(".btn").addClass("animated fadeInDown");
+    },1200);
+    setTimeout(function(){
+      $(".animar").addClass("animated fadeInDown");
+    },1600);
+    setTimeout(function(){
+      $(".animar-interno").addClass("animated fadeInDown");
+    },1200);
+  })
