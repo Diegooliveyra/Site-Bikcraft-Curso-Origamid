3ce842c31cb76748a1f3abf900feb2376a43625c
Diego Oliveira <54894831+Diegooliveyra@users.noreply.github.com>
2020-03-10 08:17:12 -0300

    Configurando formularios em php

--- formulario.js
+++ formulario.js
@@ -1,40 +1,7 @@
-$(".rslides").responsiveSlides({
-    auto: true,             // Boolean: Animate automatically, true or false
-    speed: 500,            // Integer: Speed of the transition, in milliseconds
-    timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
-  });
-
-  $(".rslides-portifolio").responsiveSlides({
-    auto: true,             // Boolean: Animate automatically, true or false
-    speed: 500,            // Integer: Speed of the transition, in milliseconds
-    timeout: 5000,          // Integer: Time between slide transitions, in milliseconds
-    pager: true,
-  });
-
-  // Animações//
-
-  Visibility.onVisible(function(){
-    setTimeout(function(){
-      $(".introducao h1, .introducao-interna h1").addClass("animated fadeInDown");
-    },400);
-    setTimeout(function(){
-      $(".quote-externo, .introducao-interna p").addClass("animated fadeInDown");
-    },800);
-    setTimeout(function(){
-      $(".btn").addClass("animated fadeInDown");
-    },1200);
-    setTimeout(function(){
-      $(".animar").addClass("animated fadeInDown");
-    },1600);
-    setTimeout(function(){
-      $(".animar-interno").addClass("animated fadeInDown");
-    },1200);
-  })
-
-  // Formulario
+// Formulario
 
 $('.formphp').on('submit', function() {
-	var emailContato = "comercial@bickraft.com.br"; // Escreva aqui o seu e-mail
+	var emailContato = ""; // Escreva aqui o seu e-mail
 
 	var that = $(this),
 			url = that.attr('action'),
